718f53669b8cb9ae8a1028b625a5cbb0aed873a5
dumah13 <koshah9@gmail.com>
2019-03-23 20:15:37 +0100

    Nowa wersja wyszukiwania

--- Wyszukiwanie.c
+++ Wyszukiwanie.c
@@ -6,15 +6,34 @@ int main(int argc, char* argv[])
 {
 	int len = 0;
 	int liczba = 0;
+    int i = 0;
 
 	printf("Podaj dlugosc tablicy.\n");
 	scanf("%d", &len);
 
+    if (len == 0){
+        printf("Błąd! Długość tablicy nie może wynosić 0");
+        return 1;
+    }
+
 	printf("Podaj szukana liczbe.\n");
 	scanf("%d", &liczba);
 
+	int D[len];
 
-	int C = wyszukaj(len, liczba);
+	printf("Tablica to: [");
+
+	for(i = 0; i < len; i++){
+		D[i] = wypelnienie(len, i); //wypelnienie tablicy D
+
+        if (i != 0)
+        	printf(" %d", D[i]);
+        else
+        	printf(",%d", D[i]);
+	}
+	printf("]\n");
+
+	int C = wyszukaj(len, liczba, D);
 
 	if (C == -1){
 		printf("Blad - liczba nie znajduje sie w tablicy.\n");
@@ -27,26 +46,13 @@ int main(int argc, char* argv[])
 int wypelnienie(int dlugosc,int i){
 	return dlugosc - i; //wypełnienie tablicy
 }
-int wyszukaj(int dlugosc, int X)
+int wyszukaj(int dlugosc, int X, int D[])
 {
-	int C,A = 0,i = 0;
+	int C,A = 0;
 	int B = dlugosc;
-	int D[dlugosc];
 
 	if (X > dlugosc || X < 0)
 		return -1;
-
-	printf("Tablica to: [");
-
-	for(i = 0; i < dlugosc; i++){
-		D[i] = wypelnienie(dlugosc, i); //wypelnienie tablicy D
-	}
-	if (i != 0)
-        	printf(" %d", D[i]);
-	else
-        	printf("%d", D[i]);
-	}
-	printf("]\n");
 
 	do{
 
